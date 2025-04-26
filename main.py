60cb0efcabda0fa5bace0accd801e0234d620fa1
Safeer Hussain <safeerhussainemail@gmail.com>
2025-04-27 01:39:49 +0500

    add soup.txt file for demo chacking and write script of request and parse through bs4

--- main.py
+++ main.py
@@ -1,1 +1,8 @@
-EnvironmentError
+from bs4 import BeautifulSoup
+import requests
+import csv
+url="https://www.amazon.com/Lululemon-Everywhere-Belt-Bag-Black/dp/B08V79CRMH"
+responce=requests.get(url)
+soup=BeautifulSoup(responce.text,'html.parser')
+with open("soup.txt",'w')as file:
+    file.write(soup.prettify())
